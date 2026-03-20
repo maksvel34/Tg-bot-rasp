a0e891f37ca6768bae44c73110ca1b93f6c1bb26
maksvel34 <157584324+maksvel34@users.noreply.github.com>
2026-03-20 11:57:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ from aiogram.enums import ChatType
 # ======================
 # ⚙ НАСТРОЙКИ
 # ======================
-TOKEN = "8628918090:AAELRmJll0PEosBm9YmicX5P1oXcAsHx4U0"
+TOKEN = "8628918090:AAEy7lzzzd_KX63b5lyGm_6-1WoKDAgivdc"
 # 🔧 Впишите сюда ID админов
 ADMIN_ID = [1071264428, 7237228038, 5301082618]
 EVEN_WEEK_START = date(2026, 2, 9)
